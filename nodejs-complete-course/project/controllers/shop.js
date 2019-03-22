31f56e593b9adb7f4667e95dbe38e93d35ce1343
suryadatta <suryadattatangirala@outlook.com>
2019-03-22 12:14:57 +0530

    fetching the products

--- nodejs-complete-course/project/controllers/shop.js
+++ nodejs-complete-course/project/controllers/shop.js
@@ -1,8 +1,9 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
+      console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
@@ -37,7 +38,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
