409922753af9412bae981c75ba7f138ba24cc746
suryadatta <suryadattatangirala@outlook.com>
2019-03-22 13:59:12 +0530

    updateing the products

--- nodejs-complete-course/project/controllers/admin.js
+++ nodejs-complete-course/project/controllers/admin.js
@@ -38,7 +38,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    // Product.findById(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -60,15 +59,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+  .then(product=>{
+    product.title = updatedTitle,
+    product.price = updatedPrice,
+    product.description = updatedDesc,
+    product.imageUrl = updatedImageUrl
+   return product.save()
+  })
+ 
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -77,7 +76,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
