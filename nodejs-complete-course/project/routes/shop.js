61341c812aa7e95e8a7e1c825d0632f25a9ed9dd
suryadatta <suryadattatangirala@outlook.com>
2019-03-23 06:47:42 +0530

    deleting the items in the cart

--- nodejs-complete-course/project/routes/shop.js
+++ nodejs-complete-course/project/routes/shop.js
@@ -16,7 +16,7 @@ router.get('/products/:productId', shopController.getProduct);
 
  router.post('/cart', shopController.postCart);
 
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+ router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 // router.post('/create-order', shopController.postOrder);
 
