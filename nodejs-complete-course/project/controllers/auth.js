1e9d041f440ca6598a49e7d1535d64c6f8195768
suryadatta <suryadattatangirala@outlook.com>
2019-03-25 17:24:55 +0530

    setting a cookies

--- nodejs-complete-course/project/controllers/auth.js
+++ nodejs-complete-course/project/controllers/auth.js
@@ -1,12 +1,17 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get('Cookie')
+    .split(';')[1]
+    .trim()
+    .split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
-    res.redirect('/');
-  };
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
+};
