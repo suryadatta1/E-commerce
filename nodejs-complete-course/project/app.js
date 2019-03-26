2d453aeede03f633fc8132fd36b0fedf72155531
suryadatta <suryadattatangirala@outlook.com>
2019-03-26 12:34:58 +0530

    authenticated with sessions

--- nodejs-complete-course/project/app.js
+++ nodejs-complete-course/project/app.js
@@ -4,20 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-
-const  mongoDbStore = require('connect-mongodb-session')(session);
+const MongoDBStore = require('connect-mongodb-session')(session);
+const port = process.env.PORT||5000;
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = "mongodb://surya:surya1234@ds121406.mlab.com:21406/shop"
+const MONGODB_URI =
+  'mongodb://surya:surya1234@ds121406.mlab.com:21406/shop';
 
 const app = express();
-const store = new mongoDbStore({
+const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection:'sessions' 
+  collection: 'sessions'
 });
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -29,14 +29,19 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({ secret: 'my secret', 
-  resave: false, 
-  saveUninitialized: false,
-  store:store 
-})
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
+
 app.use((req, res, next) => {
-  User.findById('5c94b7071dfba403f0322e1a')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -50,13 +55,12 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    MONGODB_URI,{ 
-      useNewUrlParser: true 
-    })
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true 
+})
   .then(result => {
-    User.findOne().then(user => {
+    User.findOne()
+    .then(user => {
       if (!user) {
         const user = new User({
           name: 'Max',
@@ -66,11 +70,13 @@ mongoose
           }
         });
         user.save();
-      }
+     }
+     console.log("mlab connected");
     });
+    
     app.listen(5000,()=>{
-      console.log("server running on port 5000")
-    });
+      console.log(`server running on port ${port}`);
+    })
   })
   .catch(err => {
     console.log(err);
--- nodejs-complete-course/project/controllers/auth.js
+++ nodejs-complete-course/project/controllers/auth.js
@@ -1,10 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  //   const isLoggedIn = req
-  //     .get('Cookie')
-  //     .split(';')[0]
-  //     .trim()
-  //     .split('=')[0] === 'true';
-  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -12,13 +8,31 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
-exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
 };
 
+exports.postLogin = (req, res, next) => {
+  User.findById('5c94b7071dfba403f0322e1a')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err)=>{
+  req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
   });
